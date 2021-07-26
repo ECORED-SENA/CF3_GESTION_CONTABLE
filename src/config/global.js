a46e6d7bf3b1d50b7d248072516f4fca8cbdfdb5
WalterRoaSerrano <walter.roas@misena.edu.co>
2021-07-26 11:57:34 -0500

    material.zip updated

--- src/config/global.js
+++ src/config/global.js
@@ -104,7 +104,7 @@ export default {
       {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
-        download: 'downloads/material.zip',
+        download: 'downloads/material2.zip',
       },
       {
         icono: 'far fa-registered',
